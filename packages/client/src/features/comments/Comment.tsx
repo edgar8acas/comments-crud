bcc8a11b85b943aaeacd82c3cad98b50be359f34
Edgar Ochoa <eochoa3@ucol.mx>
2021-01-29 18:12:44 -0600

    add comment css file

--- packages/client/src/features/comments/Comment.tsx
+++ packages/client/src/features/comments/Comment.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+import "./Comment.css";
 export interface CommentInterface {
   id: number;
   description: string;
@@ -12,7 +14,7 @@ export interface CommentProps {
 
 export const Comment: React.FC<CommentProps> = ({ comment }) => {
   return (
-    <div style={{}}>
+    <div className="Comment">
       <h3>{comment.author}</h3>
       <p>{comment.description}</p>
     </div>
