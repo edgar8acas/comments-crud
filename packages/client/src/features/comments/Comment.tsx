c4af1889af3e5d6a621f774255a1d01c656223e9
Edgar Ochoa <eochoa3@ucol.mx>
2021-01-30 00:42:12 -0600

    change between read/edit in comment

--- packages/client/src/features/comments/Comment.tsx
+++ packages/client/src/features/comments/Comment.tsx
@@ -12,15 +12,36 @@ export interface CommentProps {
   comment: CommentInterface;
 }
 
+type Mode = "read" | "edit";
+
 export const Comment: React.FC<CommentProps> = ({ comment }) => {
+  const [mode, setMode] = React.useState<Mode>("read");
+
+  let commentDescription;
+  let commentActions;
+  if (mode === "read") {
+    commentDescription = <p>{comment.description}</p>;
+    commentActions = (
+      <>
+        <button onClick={() => setMode("edit")}>Edit</button>
+        <button>Delete</button>
+      </>
+    );
+  } else {
+    commentDescription = <textarea value={comment.description}></textarea>;
+    commentActions = (
+      <>
+        <button onClick={() => setMode("read")}>Cancel</button>
+        <button>Save</button>
+      </>
+    );
+  }
+
   return (
     <div className="Comment">
       <h4>{comment.author}</h4>
-      <p>{comment.description}</p>
-      <div className="Comment-actions">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
+      {commentDescription}
+      <div className="Comment-actions">{commentActions}</div>
     </div>
   );
 };
